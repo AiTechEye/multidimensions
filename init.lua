a41af089638ee994f62b1b6fdcdd59bf8a0c9599
AiTechEye <40591179+AiTechEye@users.noreply.github.com>
2019-07-22 23:35:49 +0200

    Add files via upload

--- init.lua
+++ init.lua
@@ -4,21 +4,13 @@ multidimensions={
 	max_distance_chatt=800,
 	limited_chat=true,
 	limeted_nametag=true,
-	teleports=true,
-	craftable=true,
 	remake_home=true,
 	remake_bed=true,
 	dimensions={},
 	user={},
-	levels={{4,994},{994,998},{998,1000}},
-
-
 	craftable_teleporters=true,
-
 	dim_num=1,
 	registered_dimensions={},
-
-
 	map={
 		offset=0,
 		scale=1,
@@ -29,21 +21,7 @@ multidimensions={
 		lacunarity=1,
 		flags="absvalue",
 	},
-
-	map2={
-		offset=0,
-		scale=1,
-		spread={x=350,y=18,z=350},
-		seeddiff=24,
-		octaves=3,
-		persist=0.6,
-		lacunarity=2,
-		flags="absvalue",
-	},
-
-
 }
-
 
 dofile(minetest.get_modpath("multidimensions") .. "/api.lua")
 dofile(minetest.get_modpath("multidimensions") .. "/dimensions.lua")
